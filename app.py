9f694e94eb29288c63444a4507072de1cc6c8168
24311-lang <24311@daehyun.hs.kr>
2026-04-02 08:57:58 +0900

    Fix missing parenthesis in app.run()

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ str(random.randint(1,10))
     return jsonify(response)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000, debug=True
+  app.run(host='0.0.0.0', port=5000, debug=True)
